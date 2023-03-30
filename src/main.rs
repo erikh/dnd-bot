ef536025c6fc1a77237d758cf61fae8b31d6f956
Erik Hollensbe <git@hollensbe.org>
2023-03-29 21:18:23 -0700

    remove copypasta
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- src/main.rs
+++ src/main.rs
@@ -48,10 +48,6 @@ struct Handler;
 impl EventHandler for Handler {
     async fn message(&self, ctx: Context, msg: Message) {
         if msg.content.starts_with("!roll") {
-            // Sending a message can fail, due to a network error, an
-            // authentication error, or lack of permissions to post in the
-            // channel, so log to stdout when some error happens, with a
-            // description of it.
             if let Err(why) = msg.channel_id.say(&ctx.http, roll(msg.content)).await {
                 println!("Error sending message: {:?}", why);
             }
